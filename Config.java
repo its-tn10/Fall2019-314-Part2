8347d67ace2bf9f055159d44a9e5a09313547443
Tien Tavu <its.tn10@tamu.edu>
2019-11-29 20:17:11 -0600

    Began implementation for FileAccess

--- Config.java
+++ Config.java
@@ -3,6 +3,8 @@
 public class Config {
   public final static String DATAPATH = "data/"; // If you don't know what the static keyword does, you better go look it up now.
   // Prime file: One prime per line.
+  public final static String PRIMEFILE = "primes.txt";
   // Cross file: Two primes per line, separated by a comma.
+  public final static String CROSSFILE = "crosses.txt";
   public final static String APPLICATIONNAME = "Hexagon Cross for Less"; 
 }
--- FileAccess.java
+++ FileAccess.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -10,20 +11,55 @@ import java.util.Scanner;
 public class FileAccess {
   
   public static boolean loadPrimes(Primes primes, String filename) {
-		return true;
+	  try {
+		  Scanner scanner = new Scanner(new File(Config.DATAPATH + filename));
+		  // Loop through each prime and append to primes class
+		  while (scanner.hasNextBigInteger())
+			  primes.addPrime(scanner.nextBigInteger());
+		  
+		  return true; // No Errors
+	  } catch (Exception e) {
+		  e.printStackTrace();
+	  }
+	  return false;
   }
   
   public static boolean loadCrosses(Primes primes, String filename) {
-    return true;
-	}
-  
-  public static boolean savePrimes(Primes primes, String filename)
-  {
-  	return true;
+	  try {
+		  Scanner scanner = new Scanner(new File(Config.DATAPATH + filename));
+		  // Loop through each prime and append to primes class
+		  while (scanner.hasNextLine()) {
+			  Scanner line = new Scanner(scanner.nextLine());
+			  line.useDelimiter(","); // Each prime is separated by ',' so set it as delimiter for next() functions
+			  primes.addCross(new Pair<BigInteger>(line.nextBigInteger(), line.nextBigInteger()));
+		  }
+		  
+		  return true; // No Errors
+	  } catch (Exception e) {
+		  e.printStackTrace();
+	  }
+	  return false;
   }
   
-  public static boolean saveCrosses(Primes primes, String filename)
-  {
-  	return true;
+  // TODO: Ask about for-each iterables
+  
+  public static boolean savePrimes(Primes primes, String filename) {
+	  try {
+		  PrintWriter file = new PrintWriter(Config.DATAPATH + filename);
+		  
+	  } catch (Exception e) {
+		  e.printStackTrace();
+	  }
+	  return false;
+  }
+  
+  public static boolean saveCrosses(Primes primes, String filename) {
+	  try {
+		  PrintWriter file = new PrintWriter(Config.DATAPATH + filename);
+		  
+	  } catch (Exception e) {
+		  e.printStackTrace();
+	  }
+	  return false;
   }
 }
